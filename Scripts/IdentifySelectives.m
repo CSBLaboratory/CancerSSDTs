d5e6e44d8778346a3bda123e78156c86486a9ac5
Mehdi Dehghan Manshadi <manshadi1994@gmail.com>
2024-03-03 23:28:09 +0330

    Updated files and folders

--- Scripts/IdentifySelectives.m
+++ Scripts/IdentifySelectives.m
@@ -1,14 +1,25 @@
-clc
-close
-clear
+% This script is used to iterate over a list of tissues and find
+% strictly-selective synthetic lethal (SL) targets for each tissue using the
+% FindSSDTs function.
 
+clc 
+close 
+clear 
+
+% Uncomment the line below to change the CobraSolver to 'ibm_cplex' with 'lp' as the solver
 % changeCobraSolver('ibm_cplex', 'lp');
 
+% Define a list of tissues to consider
 ListOfTissues = {'COAD'; 'BRCA'; 'LUAD'; 'LIHC'; 'LUSC'; 'UCEC'; 'HNSC'; 'STAD'; 'KIRC'; 'PRAD'; 'KICH'; 'THCA'; 'KIRP'};
 
+% Specify the maximum cardinality of SDL sets to consider
 MaxCardinality = 4;
+
+% Iterate over each tissue in the list
 for i = 1 : length(ListOfTissues)
-    TargetTissue = ListOfTissues{i};
+    TargetTissue = ListOfTissues{i}; % Select the current tissue as the target tissue
+    % Call the FindSSDTs function to identify SDL targets for the current tissue
     SSDTs = FindSSDTs(TargetTissue, ListOfTissues, MaxCardinality);
+    % Save the identified SDL targets for the current tissue to a .mat file
     save([TargetTissue, '_SSDTs.mat'], 'SSDTs')
 end
